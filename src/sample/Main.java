d1cbfbc2d0615e6c936147988bc55b08a506e43a
katorek <wojtasj1996@gmail.com>
2016-10-06 20:02:31 +0200

    Add F9 key to start/stop

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,13 +1,10 @@
 package sample;
 
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-import javafx.fxml.FXMLLoader;
 import javafx.geometry.Pos;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.input.KeyCode;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
@@ -16,11 +13,10 @@ import java.awt.event.KeyEvent;
 
 public class Main extends Application {
 
+    private final String s1 = "Press to Start(F9)", s2 = "Press to Stop(F9)";
     private Button start;
     private Robot robot;
-    private int delay = 0;
     private boolean started = false;
-    private String s1 = "Press to Start", s2 = "Press to Stop";
 
     public static void main(String[] args) {
         launch(args);
@@ -28,42 +24,23 @@ public class Main extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-
         start = new Button(s1);
-        start.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent event) {
+        start.setOnAction(event -> {
+            Runnable r = this::start;
+            start.setText((start.getText().equals(s1)) ? s2 : s1);
+            new Thread(r).start();
+        });
+
+
+        GridPane grid = new GridPane();
+        grid.setOnKeyReleased(event -> {
+            if (event.getCode() == KeyCode.F9) {
                 Runnable r = this::start;
                 start.setText((start.getText().equals(s1)) ? s2 : s1);
                 new Thread(r).start();
             }
-
-            private void start() {
-                if (robot == null) {
-                    try {
-                        robot = new Robot();
-                    } catch (AWTException e) {
-                        e.printStackTrace();
-                    }
-                }
-                started = !started;
-                GraphicsDevice gd = GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice();
-                int height = gd.getDisplayMode().getHeight();
-                int width = gd.getDisplayMode().getWidth();
-                while (started) {
-                    robot.keyPress(KeyEvent.VK_W);
-                    robot.mouseMove(width * 3 / 4, height / 2);
-                    robot.delay(100);
-                    robot.keyRelease(KeyEvent.VK_W);
-                }
-//                robot.keyRelease(KeyEvent.VK_D);
-//                robot.keyRelease(KeyEvent.VK_W);
-                robot.delay(delay);
-            }
         });
 
-        GridPane grid = new GridPane();
         grid.add(start, 0, 0);
         grid.setVgap(10);
         grid.setHgap(10);
@@ -73,5 +50,25 @@ public class Main extends Application {
         stage.setScene(new Scene(grid, 50, 50));
         stage.setResizable(false);
         stage.show();
+    }
+
+    private void start() {
+        if (robot == null) {
+            try {
+                robot = new Robot();
+            } catch (AWTException e) {
+                e.printStackTrace();
+            }
+        }
+        started = !started;
+        GraphicsDevice gd = GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice();
+        int height = gd.getDisplayMode().getHeight();
+        int width = gd.getDisplayMode().getWidth();
+        while (started) {
+            robot.keyPress(KeyEvent.VK_W);
+            robot.mouseMove(width * 3 / 4, height / 2);
+            robot.delay(100);
+            robot.keyRelease(KeyEvent.VK_W);
+        }
     }
 }
